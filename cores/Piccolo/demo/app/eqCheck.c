6632b5ed67517dc8dbeef970bdcd8afb228982e6
tinochinamora <ttc2@princetonl.edu>
2020-04-29 02:09:46 +0000

    EQ check for RTL and ILA sims

--- cores/Piccolo/demo/app/eqCheck.c
+++ cores/Piccolo/demo/app/eqCheck.c
@@ -11,6 +11,7 @@ int main(int argc, char** argv) {
     int var_counter;
     int var_match;
     int i;
+    int discard;
 
     var_match = TRUE;
 
@@ -20,8 +21,8 @@ int main(int argc, char** argv) {
     FILE *fp2 = fopen("ila_vars.txt", "r");
 
     while (!(feof(fp1) & feof(fp2))) {
-        rtl_var = fscanf(fp1, "%d", &i);
-        ila_var = fscanf(fp2, "%d", &i);
+        fscanf(fp1, "%d", &rtl_var);
+        fscanf(fp2, "%d", &ila_var);
   
         var_match = var_match && (rtl_var == ila_var);
     }
@@ -37,9 +38,11 @@ int main(int argc, char** argv) {
     FILE *fprint2 = fopen("ila_vars.txt", "r");
 
     printf("REGISTER VALUES\n");
+    fscanf(fprint1, "%d", &discard);
+    fscanf(fprint2, "%d", &discard);
     for (i = 0;  i < 32; i++) {
-        rtl_var = fscanf(fprint1, "%d", &i);
-        ila_var = fscanf(fprint2, "%d", &i);
+        fscanf(fprint1, "%d", &rtl_var);
+        fscanf(fprint2, "%d", &ila_var);
 
         printf("RTL reg%d -> %d and ILA reg%d -> %d\n", i, rtl_var, i, ila_var);
     }
